f46ec47aa8ccaf267b84321225948109b96d8323
Emman Calabroso <emman.calabroso@chromedia.com>
2017-04-10 14:32:28 +0800

    add initial files

---
+++ js/app.js
@@ -0,0 +1,35 @@
+$(document).foundation();
+
+function moveHeading () { 
+	$("#intro").velocity ( { translateY: 0 }, [ 400, 30 ], 1800 );
+}
+
+function slideInItems () {
+	$(".highlight").velocity ( { translateY: 0 }, [ 400, 30], 1800 );
+	
+	$(".slide-in").velocity ( "transition.slideUpIn", { stagger: 80, drag: !0 } );
+}
+
+$("#intro").velocity ( { 
+	translateY: ( $(window).outerHeight() - $("#intro").height() ) / 2 ,
+	opacity: 1 
+	}, 0 
+);
+
+$(".highlight").velocity ({
+	translateY: $(window).height()
+	}, 0
+);
+
+$("#intro").blast({ 
+	delimiter:"character"
+}).velocity("transition.slideUpIn", { 
+	display: null,
+	stagger: 25, 
+	duration: 600,
+	complete: function () {
+		setTimeout(moveHeading, 10);
+		setTimeout(slideInItems, 10);
+	}
+});
+
