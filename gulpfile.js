4138f8338886db46dd05ed784820545f2c4c6a80
Emman Calabroso <emman.calabroso@chromedia.com>
2018-05-10 19:00:01 +0800

    push files

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+var gulp = require('gulp');
+var $ 	 = require('gulp-load-plugins')();
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+
+var sassPaths = [
+	'node_modules/bootstrap/scss'
+];
+
+gulp.task('sass', function () {
+	return gulp.src('public/scss/styles.scss')
+	.pipe($.sass({
+		includePaths: sassPaths,
+		outputStyle: 'compressed'
+	}).on('error', $.sass.logError))
+	.pipe(gulp.dest('public/css'))
+	.pipe(reload({ stream: true }));
+});
+
+gulp.task('serve', function () {
+	browserSync({
+		server: {
+			baseDir: ['./', 'public'],
+		}
+	});
+
+	gulp.watch(['public/scss/**/*.scss'], ['sass']);
+	gulp.watch(['public/*.html']).on('change', browserSync.reload);
+	
+});
+
+gulp.task('default', ['sass'], function() {
+	gulp.watch(['public/*.html'], ['public/scss/**/*.scss'], ['sass']);
+});
